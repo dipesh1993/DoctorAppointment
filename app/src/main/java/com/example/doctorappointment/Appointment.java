1a2e6e4fe5045249f4340c82143fc2fca863c444
NomanBaigA <49509445+NomanBaigA@users.noreply.github.com>
2020-10-01 19:00:09 +0530

    Update Appointment.java

--- app/src/main/java/com/example/doctorappointment/Appointment.java
+++ app/src/main/java/com/example/doctorappointment/Appointment.java
@@ -1,6 +1,5 @@
 package com.example.doctorappointment;
 
-
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -21,62 +20,67 @@ public class Appointment extends AppCompatActivity {
     TabLayout tabLayout;
     Context context;
     FragmentManager fragmentManager;
-    Fragment fragment=null;
+    Fragment fragment = null;
     FragmentTransaction fragmentTransaction;
 //    SessionManager sessionManager;
-    String mId,name1,designation1;
+    String mId, name1, designation1;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_appointment);
-        context=this.getApplicationContext();
+        context = this.getApplicationContext();
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        
 //        sessionManager=new SessionManager(this);
 //        HashMap<String, String> user=sessionManager.getUserDetail();
 //        mId=user.get(sessionManager.ID);
 //        name1=getIntent().getExtras().getString("name");
 //        designation1=getIntent().getExtras().getString("designation");
 
-        frameLayout=(FrameLayout)findViewById(R.id.simpleFrameLayout);
-        tabLayout=(TabLayout)findViewById(R.id.simpleTabLayout);
+        frameLayout = (FrameLayout)findViewById(R.id.simpleFrameLayout);
+        tabLayout = (TabLayout)findViewById(R.id.simpleTabLayout);
+        
 //        ActionBar actionBar=((MainActivity ) fragment.getActivity()).getSupportActionBar();
 
-        TabLayout.Tab firstTab=tabLayout.newTab();
+        TabLayout.Tab firstTab = tabLayout.newTab();
         firstTab.setText("Book Appointment");
+        
 //        firstTab.setIcon(R.drawable.newsicon);
         tabLayout.addTab(firstTab);
 
-        TabLayout.Tab secondTab=tabLayout.newTab();
+        TabLayout.Tab secondTab = tabLayout.newTab();
         secondTab.setText("My Appointments");
+        
 //        secondTab.setIcon(R.drawable.visit);
         tabLayout.addTab(secondTab);
 
-
-
-        fragment=new Booking();
+        fragment = new Booking();
         fragmentManager = getSupportFragmentManager();
         fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(R.id.simpleFrameLayout, fragment);
         fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
         fragmentTransaction.commit();
+        
         tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
 
-                switch (tab.getPosition())
-                {
+                switch (tab.getPosition()){
+                       
                     case 0:
-                        fragment=new Booking();
+                        fragment = new Booking();
                         break;
                     case 1:
-                        fragment=new MyBookings();
+                        fragment = new MyBookings();
                         break;
                 }
 
-                FragmentManager fm=getSupportFragmentManager();
-                FragmentTransaction ft=fm.beginTransaction();
+                FragmentManager fm = getSupportFragmentManager();
+                FragmentTransaction ft = fm.beginTransaction();
                 ft.replace(R.id.simpleFrameLayout,fragment);
                 ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
                 ft.commit();
@@ -84,16 +88,12 @@ public class Appointment extends AppCompatActivity {
 
             @Override
             public void onTabUnselected(TabLayout.Tab tab) {
-
             }
 
             @Override
             public void onTabReselected(TabLayout.Tab tab) {
-
             }
-
         });
-
     }
 
 //    @Override
@@ -122,11 +122,13 @@ public class Appointment extends AppCompatActivity {
 //        }
 //        return true;
 //    }
+    
     @Override
     public boolean onSupportNavigateUp(){
         finish();
         return true;
     }
+    
     @Override
     public void onBackPressed() {
 //        new AlertDialog.Builder(this)
@@ -143,6 +145,4 @@ public class Appointment extends AppCompatActivity {
         finish();
 
     }
-
-
 }
